6c79d3d38a18c9aca73a1a58175fa8b3bc2ac4d1
Takuyann <cl_s3206541126@soshi.jp>
2023-01-17 15:28:37 +0900

    完成

--- Camera Kanzi/ViewController.swift
+++ Camera Kanzi/ViewController.swift
@@ -10,14 +10,18 @@ import MLKit
 import MLKitTextRecognitionJapanese
 import MLKitTextRecognitionCommon
 import AVFoundation
+import CropViewController
 
-    class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,CropViewControllerDelegate{
         @IBOutlet var cameraImageView: UIImageView!
+        @IBOutlet var honyakukekka: UILabel!
         
         let japaneseOptions = JapaneseTextRecognizerOptions()
         let japaneseTextRecognizer = TextRecognizer.textRecognizer(options: JapaneseTextRecognizerOptions())
+        var kanzihonyaku: String = ""
        // let image = VisionImage(image: UIImage(named: "159905")!)
         override func viewDidLoad() {
+            honyakukekka.adjustsFontSizeToFitWidth = true
             super.viewDidLoad()
 //            japaneseTextRecognizer.process(image) { result, error in
 //                guard error == nil, let result = result else { return }
@@ -26,13 +30,53 @@ import AVFoundation
             // Do any additional setup after loading the view.
         }
        // func scan(sampleBuffer: CMSampleBuffer){
+    //カメラで撮ったのをひらがなにする{
         @IBAction func botan(){
             let image = VisionImage(image: cameraImageView.image!)
             japaneseTextRecognizer.process(image) { result, error in
                 guard error == nil, let result = result else { return }
+                self.kanzihonyaku = result.text
                 print("resultText: \(result.text)")
             }
-        }
+            var request = URLRequest(url: URL(string: "https://labs.goo.ne.jp/api/hiragana")!)
+                    request.httpMethod = "POST"
+                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    //POSTするデータをURLRequestに持たせる
+                    let postData = PostData(app_id: "6937c927439be919b9c5add398fcd7a18307788779ce23b4181eb3a9c5435b6f", request_id: "record003", sentence: kanzihonyaku, output_type: "hiragana")
+                    guard let uploadData = try? JSONEncoder().encode(postData) else {
+                        print("json生成に失敗しました")
+                        return
+                    }
+                    request.httpBody = uploadData
+                    //APIへPOSTしてresponseを受け取る
+                    let task = URLSession.shared.uploadTask(with: request, from: uploadData) {
+                        data, response, error in
+                        if let error = error {
+                            print ("error: \(error)")
+                            return
+                        }
+                        guard let response = response as? HTTPURLResponse,
+                            (200...299).contains(response.statusCode) else {
+                                print ("server error")
+                                return
+                        }
+                        if response.statusCode == 200 {
+                            guard let data = data, let jsonData = try? JSONDecoder().decode(Rubi.self, from: data) else {
+                                print("json変換に失敗しました")
+                                return
+                            }
+                            print(jsonData.converted)
+                            DispatchQueue.main.async {
+                                self.honyakukekka.text = jsonData.converted
+                            }
+                        } else {
+                            print("サーバエラー ステータスコード: \(response.statusCode)\n")
+                        }
+                    }
+                    task.resume()
+                }
+        //   }
+        //画像のテキストを認識する
         func imageOrientation(deviceOrientaton: UIDeviceOrientation, cameraPosition: AVCaptureDevice.Position) -> UIImage.Orientation{
             switch deviceOrientaton {
             case .portrait:
@@ -75,6 +119,14 @@ import AVFoundation
             print("Camera not available")
         }
     }
+        func cropViewController(_ cropViewController: CropViewController, didCropToImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+                updateImageViewWithImage(image, fromCropViewController: cropViewController)
+            }
+                
+            func updateImageViewWithImage(_ image: UIImage, fromCropViewController cropViewController: CropViewController) {
+                cameraImageView.image = image
+                cropViewController.dismiss(animated: true, completion: nil)
+            }
     //カメラ、カメラロールを使った時に選択した画像をアプリ内に表示するためのメソッド
     func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true)
@@ -82,11 +134,37 @@ import AVFoundation
             print("Image not found.")
             return
         }
-        cameraImageView.image = image
+        //cameraImageView.image = image
+        let cropController = CropViewController(croppingStyle: .default, image: image)
+                cropController.delegate = self
+                cropController.customAspectRatio = CGSize(width: 100, height: 100)
+                
+                //今回は使わないボタン等を非表示にする。
+                cropController.aspectRatioPickerButtonHidden = true
+                cropController.resetAspectRatioEnabled = true
+                cropController.rotateButtonsHidden = true
+                
+                //cropBoxのサイズを固定する。
+                cropController.cropView.cropBoxResizeEnabled = true
+                //pickerを閉じたら、cropControllerを表示する。
+                picker.dismiss(animated: true) {
+                    self.present(cropController, animated: true, completion: nil)
+                }
     }
 //        func japaneseTextRecognizer.process(image); {result, error in
 //            guard error == nil, let result = result else { return }
 //            print("resultText: \(result.text)")
 //        }
+}
+struct Rubi:Codable {
+    var request_id: String
+    var output_type: String
+    var converted: String
+}
+struct PostData: Codable {
+    var app_id:String
+    var request_id: String
+    var sentence: String
+    var output_type: String
 }
 
